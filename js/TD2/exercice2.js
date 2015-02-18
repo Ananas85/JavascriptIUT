46677f68072a6c6d74444134d1a4e40610726a8a
anaelChardan <anael.chardan@gmail.com>
2015-02-18 16:38:44 +0100

    creation exercice 4

--- js/TD2/exercice2.js
+++ js/TD2/exercice2.js
@@ -1,24 +1,24 @@
 
-    var musiciens;
+var musiciens;
 
-    function loadListe() {
-        var list = document.getElementById( 'liste' );
-        for( var i = 0; i < musiciens.length; ++i ) {
-            var li = document.createElement( 'li' );
-            var node = document.createTextNode( musiciens[i].Nom_Musicien + ", " + musiciens[i].Prénom_Musicien );
-            li.appendChild( node );
-            list.appendChild( li );
-        }
+function loadListe() {
+    var list = document.getElementById( 'liste' );
+    for( var i = 0; i < musiciens.length; ++i ) {
+        var li = document.createElement( 'li' );
+        var node = document.createTextNode( musiciens[i].Nom_Musicien + ", " + musiciens[i].Prénom_Musicien );
+        li.appendChild( node );
+        list.appendChild( li );
     }
+}
 
-    function loadTable() {
-        musiciens = JSON.parse( document.getElementById( 'music' ).text );
-    }
+function loadTable() {
+    musiciens = JSON.parse( document.getElementById( 'music' ).text );
+}
 
-    function display2() {
-        loadTable();
-        loadListe();
-    }
+function display2() {
+    loadTable();
+    loadListe();
+}
 
 
 
