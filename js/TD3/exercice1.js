eb38181ea7735be5add4f587b83d4a219233b632
anaelChardan <anael.chardan@gmail.com>
2015-03-04 10:46:37 +0100

    some works

--- js/TD3/exercice1.js
+++ js/TD3/exercice1.js
@@ -3,7 +3,7 @@ var requestAjax;
 function connect(page) {
     requestAjax = new XMLHttpRequest();
     if(requestAjax != null) {
-        requestAjax.open("GET", page, true)
+        requestAjax.open("GET", page, true);
         requestAjax.onreadystatechange = handler;
         requestAjax.send();
     } else {
@@ -18,6 +18,7 @@ function handler(){
         }
     }
 }
+
 function call(){
     connect("donnees.xml");
 }
