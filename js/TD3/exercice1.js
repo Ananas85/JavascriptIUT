10020cf9b0521fc2e1f07de8b81541db30079ef1
anaelChardan <anael.chardan@gmail.com>
2015-03-04 09:26:51 +0100

    Ajout du JS et du HTML ex1 TD3

--- js/TD3/exercice1.js
+++ js/TD3/exercice1.js
@@ -1,5 +1,36 @@
 var requestAjax;
 
 function connect(page) {
+    requestAjax = new XMLHttpRequest();
+    if(requestAjax != null) {
+        requestAjax.open("GET", page, true)
+        requestAjax.onreadystatechange = handler;
+        requestAjax.send();
+    } else {
+        window.alert("Pas de support AJAX ( XMLHTTP ) ");
+    }
+}
 
+function handler(){
+    if(requestAjax.readyState === 4){
+        if(requestAjax.status === 200 ){
+            display();
+        }
+    }
+}
+function call(){
+    connect("donnees.xml");
+}
+
+function display(){
+    var docXml = requestAjax.responseXML;
+    var responses = docXml.getElementsByTagName("NOM");
+    var text = "";
+    for( i = 0; i < responses.length; ++i ){
+        text = text + responses[i].firstChild.textContent + " ";
+    }
+
+    var divlist = document.getElementById("musiciens");
+    divlist.style.visibility = "visible";
+    divlist.value = text;
 }
