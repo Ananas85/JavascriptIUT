5d487af32b56027252c400f6282f919e50563f77
anaelChardan <anael.chardan@gmail.com>
2015-03-11 09:14:32 +0100

    plop

--- js/TD4/exercice2.js
+++ js/TD4/exercice2.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
     $('.target span').on('click', function() {
-        $(this).css('color', 'red');
-        $(this).css('border-color', 'red');
-        $(this).css('border-radius', '20px');
-        $(this).text("Bouton rouge");
+        if($(this).css('color') == "red"){
+            $(this).css('color', 'green');
+            $(this).css('border-color', 'green');
+            $(this).css('border-radius', '0px');
+            $(this).text("Bouton Vert");
+        } else {
+            $(this).css('color', 'red');
+            $(this).css('border-color', 'red');
+            $(this).css('border-radius', '20px');
+            $(this).text("Bouton Rouge");
+        }
+
     });
 });
